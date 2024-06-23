c26dfa9b3caf93eedd983788a87a95da7be1d72e
tekina96 <aniketkundu754@gmail.com>
2024-06-23 22:20:40 +0530

    2nd part done - ui design

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BiLogOut } from "react-icons/bi";
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'> 
+        <BiLogOut className='w-6 h-6 text-white cursor-pointer'/>
+		{/* {!loading ? (
+			<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+		) : (
+			<span className='loading loading-spinner'></span>
+		)} */}
+	</div>
+  )
+}
+
+export default LogoutButton
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                Login
+                <span className='text-blue-500'> Chatters</span>
+            </h1>
+
+            <form>
+                <div>
+                    <label className='label p-2'>
+                        <span className='label p-2'>Username</span>
+                    </label>
+                    <input
+						type='text'
+						placeholder='Enter username'
+						className='w-full input input-bordered h-10'
+						// value={username}
+						// onChange={(e) => setUsername(e.target.value)}
+					/>
+                </div>
+
+                <div>
+                <label className='label'>
+					<span className='text-base label-text'>Password</span>
+				</label>
+				<input
+					type='password'
+					placeholder='Enter Password'
+					className='w-full input input-bordered h-10'
+					// value={password}
+					// onChange={(e) => setPassword(e.target.value)}
+				/>
+                </div>
+                <a href="#" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                    {"Don't"} have an account ?
+                </a>
+
+                <div>
+					<button className='btn btn-block btn-sm mt-2'>  Login
+                        {/* // disabled={loading} */}
+						{/* {loading ? <span className='loading loading-spinner '></span> : "Login"} */}
+					</button>
+				</div>
+            </form>
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
