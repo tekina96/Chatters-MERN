f4725efaf996cfcbe5ce4b11e99da8e7fe6d84d2
tekina96 <aniketkundu754@gmail.com>
2024-06-28 01:12:09 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // Package import
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,13 +15,16 @@ dotenv.config();
 
 // Variables
 // const app = express();  // We comment out here because we are calling from socket.js file
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
+// Deploy
+const __dirname = path.resolve();
+// This gives us absolute path to the root file
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Welcome to my website!");
-})
+// app.get("/", (req, res) => {
+//     // root route http://localhost:5000/
+//     res.send("Welcome to my website!");
+// })
 
 // Middleware
 app.use(express.json());  // To parse the incoming requests with JSON payloads ( from req.body)
@@ -34,7 +38,16 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));  
+// static function is generally used to serve static files in our frontend applications(like HTML, CSS, JS, audio, image files etc)
+// So when we build this application and deploy it using vite, it will bundle the whole frontend codes into the dist folder, this we do for optimisation.
 
+// Now any route apart from the upper ones will render index.html file
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    // This'll send the HTML file
+});
+// This way we can run our frontend from server as well
 
 server.listen(PORT, () => {
     connectToMongoDB();
